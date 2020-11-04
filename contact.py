195f57e23302707c44653e545a3b812718951d01
Arya Basirolomi <aryabasir2001@hotmail.com>
2020-11-04 18:10:46 -0500

    Set up scope of script with group on a zoom call, everyone will add docstrings later

--- contact.py
+++ contact.py
@@ -1,3 +1,97 @@
-lsdef hello():
-    print("hello world")
-    print("goodbye world")
+class ContactBook():
+    
+    
+    def __init__(self, name, number, email, zipcode):
+        self.name = name
+        self.number = number
+        self.number = email
+        self.zipcode = zipcode
+        
+        
+    def add_contact(self):
+        """Deion
+        This method... 
+        """
+        pass
+    
+    
+    def remove_contact(self):
+        """Deion
+        """
+        pass
+    
+    def deleted_contacts(self):
+        """Joshua
+        Everything that gets deleted gets stored in here
+        But only the 5 more recent deleted contacts are stored here, 
+        
+        """
+    
+    def pull_contact(self):
+        """Yinka
+        Fine one specific contact
+        Enter name of desired contact, then 
+        
+        Example of code:
+            for cantact in contact.txt
+                if user_input == contact
+                    return contact
+        """
+        
+        
+    def update_contact(self, contact):
+        """Yinka
+        This method updates a previously existing contact. 
+        
+        Args:
+            contact(list): the output of the pull_contact method
+        
+        Raises:
+            ValueError if you are updating a contact that doesn't exist.
+            
+        Example of possible input:
+            Arya, 240-354-2848, aryabasir2001@hotmail.com, 20855)
+            
+        Ideas:
+            use regex to find #
+        """
+    
+    
+    def sort_by_name(self):
+        """Joshua
+        """
+        pass
+    
+    
+    def sort_by_zipcode(self):
+        """Joshua
+        regex sort by zipcode
+        """
+        pass
+    
+    
+    def print_all(self):
+        """Arya
+        """
+        pass
+    
+    
+    def share_contact(self):
+        """Arya 
+        logging library
+        """
+        pass
+
+    def favorites(self):
+        """Arya
+        """
+        pass
+    
+       
+def main():
+    pass
+         
+    
+def parse_args():
+        """Enter -h to see possible functions, then move on from there
+        """
