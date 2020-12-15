2486f502fc15e7f35c681ad3570e882494dfc987
Yinka Akinola <yakinola@terpmail.umd.edu>
2020-12-15 13:02:48 -0500

    Worked on test script

--- test_contact.py
+++ test_contact.py
@@ -1,2 +1,11 @@
 """ Test functions for the contact script """
 from contact import ContactBook
+
+def main():
+    """ Demonstrate use of ContactBook class. """
+    contact_1 = ContactBook(contacts.txt)
+    assert contact_1.pull_one_contact(Kanye West) == ['Kanye West', ' 657-882-1340', ' kanye4prez@yahoo.com', '12345']
+    assert contact_1.pull_one_contact(Tony Stark) == ['Tony Stark', ' 212-970-4133', ' tonystark@gmail.com', ' 90265']
+    assert contact_1.pull_one_contact(Mike Ike) == ['Mike Ike', ' 709-704-7126', ' mikeike@mikeike.com', ' 36110']
+    
+    
